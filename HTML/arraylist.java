3e282ea7117e9abe0bb1c0bf7d0ee0c0a40ae112
Shuaib <shuaib6950@gmail.com>
2024-01-07 22:01:59 +0600

    first commit

---
+++ HTML/arraylist.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+
+public class ArrayList {
+    public static void main(String[] args) {
+        ArrayList<Card> arr = new ArrayList <Integer> ();
+        arr.add(25);
+        arr.add(71);
+        arr.add(81);
+        
+        
+        for (int i = 0; i < 3; i++) {
+            System.out.printf("ID is: %d", arr.get(i));
+        }
+        
+    }
+}mkmm,,,.;.;/'\\\\			e'
